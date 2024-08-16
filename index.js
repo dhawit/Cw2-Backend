0b0c5b50fb3bb98a61f567ccb680136cac6b4d80
dhawit <tdhawit@gmail.com>
2024-08-16 22:32:03 +0545

    Update index.js

--- index.js
+++ index.js
@@ -84,5 +84,8 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+app.get("/", (req, res) => {
+    res.status(200).send("API by Dhawit");
+  });
 // exporting app
 module.exports = app;
