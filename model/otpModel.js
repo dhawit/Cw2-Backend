0684302aee61ac810b3c59404bdf8cef8742f248
dhawit <tdhawit@gmail.com>
2024-08-16 21:06:23 +0545

    Create otpModel.js

---
+++ model/otpModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const otpSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+  otp: {
+    type: String,
+    required: true,
+  },
+  isUsed: { 
+    type: Boolean, 
+    default: Date.now
+ },
+});
+
+const OTP = mongoose.model("OTP", otpSchema);
+module.exports = OTP;
